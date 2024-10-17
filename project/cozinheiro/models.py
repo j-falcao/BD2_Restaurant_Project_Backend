33f83f0e29c78789882045bb79c82602b6d2e0cd
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-10-17 17:33:27 +0100

    deleting files

--- project/cozinheiro/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Cozinheiro(models.Model):
-    id_pedido = models.ForeignKey('pedidos.Pedido', on_delete=models.CASCADE)
-    id_especialidade = models.ForeignKey(
-        'especialidade.Especialidade', on_delete=models.CASCADE)

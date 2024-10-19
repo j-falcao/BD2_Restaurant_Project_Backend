ea4f11a65319f78242e12f7b6dbb5d67efd55869
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-10-19 23:09:24 +0100

    fix

--- project/cargos/urls.py
+++ project/cargos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import ( # type: ignore
+from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    path('signin/', lambda args: print(TokenObtainPairView.as_view()), name='token_obtain_pair'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- project/project/settings.py
+++ project/project/settings.py
@@ -42,8 +42,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173/',
-    'http://127.0.0.1:5173/',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 ROOT_URLCONF = 'project.urls'

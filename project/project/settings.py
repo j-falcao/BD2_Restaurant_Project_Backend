8ad0c7b6161dff69dfbc59cee42b8dcde6347713
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-01 10:55:03 +0000

    overwritting user model for custom authentication

--- project/project/settings.py
+++ project/project/settings.py
@@ -221,3 +221,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'cargos.Utilizador'

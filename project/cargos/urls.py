8744d0f7ec5ac45074cbd325234ecd26c15d0a60
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-08 19:42:48 +0000

    update urls

--- project/cargos/urls.py
+++ project/cargos/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import login_view
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import login_view, signup_view, whoami_view
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('', whoami_view, name='whoami'),
 ]

746cf867d894a539a33177c38b1ef638743cfc74
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-08 16:55:12 +0000

    more

--- project/inventario/views.py
+++ project/inventario/views.py
@@ -3,4 +3,8 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def ingredientes_view(request):
-    return Response([{'name': 'João', 'idade': 22}])
+    return Response([
+        {'id': 1, 'name': 'João', 'idade': 22},
+        {'id': 2, 'name': 'Maria', 'idade': 24},
+        {'id': 3, 'name': 'Jose', 'idade': 23},
+    ])

edca3ba5e666f8e809836ba0b9d2bdc31356f01d
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-08 19:41:34 +0000

    adding whoami and signup routes

--- project/cargos/views.py
+++ project/cargos/views.py
@@ -2,6 +2,23 @@ from django.http import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate
+from .models import Utilizador
+
+@api_view(['POST'])
+def signup_view(request):
+    username = request.data.get("username")
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if not all([username, email, password]):
+        return JsonResponse({"error": "Todos os campos devem ser preenchidos"}, status = 400)
+    
+    user = Utilizador.objects.create_user(username, email, password)
+
+    if user is not None:
+        return JsonResponse({"message": "Utilizador criado com sucesso"})
+    else:
+        return JsonResponse({"error": "Erro ao criar o utilizador"}, status = 500)
 
 @api_view(['POST'])
 def login_view(request):
@@ -29,3 +46,12 @@ def login_view(request):
     else:
         return JsonResponse({"error": "Credenciais inválidas"}, status=401)
     
+
+@api_view(['GET'])
+def whoami_view(request):
+    # Verificar se o utilizador está autenticado
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        return JsonResponse({"username": request.user.username})
+    else:
+        return JsonResponse({"error": "Utilizador não autenticado"})

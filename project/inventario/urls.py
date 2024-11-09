ed5490371b7326e6edd3e00d6355e4fadc48519b
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-09 13:26:55 +0000

    fix

--- project/inventario/urls.py
+++ project/inventario/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ingredientes/', views.ingredientes_view),
+    path('ingredientes/', views.get_ingredientes, name='ingredientes'),
+    path('ingredientes/<str:id_ingrediente>/', views.get_ingrediente, name='ingrediente'),
+
+    path('utensilios/', views.get_utensilios, name='utensilios'),
+    path('utensilios/<str:id_utensilio>/', views.get_utensilio, name='utensilio'),
+
+    path('receitas/', views.get_receitas, name='receitas'),
+    path('receitas/<str:id_receita>/', views.get_receita, name='receita'),
+
+    path('carrinhos/', views.get_carrinhos, name='carrinhos'),
+    path('carrinhos/<str:id_carrinho>/', views.get_carrinho, name='carrinho'),
+
+    path('fornecedores/', views.get_fornecedores, name='fornecedores'),
+    path('fornecedores/<str:id_fornecedor>/', views.get_fornecedor, name='fornecedor'),
 ]
--- project/inventario/views.py
+++ project/inventario/views.py
@@ -1,15 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import db
+from . import db
 
 
 @api_view(['GET'])
-def get_igredients(request):
+def get_ingredientes(request):
     return Response(db.get_all_ingredientes())
 
 @api_view(['GET'])
 def get_ingrediente(request, id_ingrediente):
     return Response(db.get_ingrediente_by_id(id_ingrediente))
+
 
 @api_view(['GET'])
 def get_utensilios(request):
@@ -20,9 +21,28 @@ def get_utensilio(request, id_utensilio):
     return Response(db.get_utensilio_by_id(id_utensilio))
 
 
-def get_receita_by_id(request, id_receita):
+@api_view(['GET'])
+def get_receitas(request):
+    return Response(db.get_all_receitas())
+
+@api_view(['GET'])
+def get_receita(request, id_receita):
     return Response(db.get_receita_by_id(id_receita))
 
-def get_receita_by_item(request, id_item):
-    return Response(db.get_receita_by_item(id_item))
 
+@api_view(['GET'])
+def get_carrinhos(request):
+    return Response(db.get_all_carrinhos())
+
+@api_view(['GET'])
+def get_carrinho(request, id_carrinho):
+    return Response(db.get_carrinho_by_id(id_carrinho))
+
+
+@api_view(['GET'])
+def get_fornecedores(request):
+    return Response(db.get_all_fornecedores())
+
+@api_view(['GET'])
+def get_fornecedor(request, id_fornecedor):
+    return Response(db.get_fornecedor_by_id(id_fornecedor))

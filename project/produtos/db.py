0fd34ccc081e06a150dde36708819b305b28e248
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-11-29 17:08:20 +0000

    cv

--- project/produtos/db.py
+++ project/produtos/db.py
@@ -1,42 +1,42 @@
-from .models import Produto, Item, Tipo, Categoria, Opcao, Menu
+from models import *
 
 def get_all_produtos():
-    return Produto.objects.all()
+    return Produtos.objects.all()
 
 def get_produto_by_id(id_produto):
-    return Produto.objects.get(id_produto=id_produto)
+    return Produtos.objects.get(id_produto=id_produto)
 
 
 def get_all_itens():
-    return Item.objects.all()
+    return Itens.objects.all()
 
 def get_item_by_id(id_item):
-    return Item.objects.get(id_item=id_item)
+    return Itens.objects.get(id_item=id_item)
 
 
 def get_all_tipos():
-    return Tipo.objects.all()
+    return Tipos.objects.all()
 
 def get_tipo_by_id(id_tipo):
-    return Tipo.objects.get(id_tipo=id_tipo)
+    return Tipos.objects.get(id_tipo=id_tipo)
 
 
 def get_all_categorias():
-    return Categoria.objects.all()
+    return Categorias.objects.all()
 
 def get_categoria_by_id(id_categoria):
-    return Categoria.objects.get(id_categoria=id_categoria)
+    return Categorias.objects.get(id_categoria=id_categoria)
 
 
 def get_all_opcoes():
-    return Opcao.objects.all()
+    return Opcoes.objects.all()
 
 def get_opcao_by_id(id_opcao):
-    return Opcao.objects.get(id_opcao=id_opcao)
+    return Opcoes.objects.get(id_opcao=id_opcao)
 
 
 def get_all_menus():
-    return Menu.objects.all()
+    return Menus.objects.all()
 
 def get_menu_by_id(id_menu):
-    return Menu.objects.get(id_menu=id_menu)
+    return Menus.objects.get(id_menu=id_menu)

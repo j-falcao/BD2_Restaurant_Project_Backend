186c5ce223cb48d40286e78f9dccfc65d1ba1a0d
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-12-11 09:17:30 +0000

    added instrucoesingredientes and instrucoesingredientes views

--- project/database/scripts/create_views.sql
+++ project/database/scripts/create_views.sql
@@ -105,7 +105,15 @@ SELECT ppio.*, pp.id_produto FROM pedidosprodutositensopcoes ppio
 JOIN pedidosprodutos_view pp ON pp.id_pedido_produto = ppio.id_pedido_produto;
 
 CREATE OR REPLACE VIEW instrucoes_view AS
-SELECT * FROM instrucoes;
+SELECT i.*, ii.descricao AS descricao_ingrediente, iu.descricao AS descricao_utensilio FROM instrucoes i
+JOIN instrucoesingredientes ii ON ii.id_instrucao = instrucoes.id_instrucao
+JOIN instrucoesutensilios iu ON iu.id_instrucao = instrucoes.id_instrucao
+
+CREATE OR REPLACE VIEW instrucoesingredientes_view AS
+SELECT * FROM instrucoesingredientes;
+
+CREATE OR REPLACE VIEW instrucoesutensilios_view AS
+SELECT * FROM instrucoesutensilios;
 
 CREATE OR REPLACE VIEW utensiliosreceitas_view AS
 SELECT ur.*, u.nome FROM utensiliosreceitas ur

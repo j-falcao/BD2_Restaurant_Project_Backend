c348efce5feba21edbd63ea51146d0b1840e7d38
j-falcao <100717304+j-falcao@users.noreply.github.com>
2024-12-30 15:27:38 +0000

    a

--- project/autenticacao/models.py
+++ project/autenticacao/models.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-
 
 class Cargos(models.Model):
     id_cargo = models.AutoField(primary_key=True)
@@ -14,41 +15,43 @@ class Cargos(models.Model):
     def __str__(self):
         return self.designacao
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field is required')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class Utilizadores(models.Model):
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+
+class Utilizadores(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
-    cargos = models.ManyToManyField(Cargos, through='UtilizadoresCargos', related_name='utilizadores')
-    url_imagem = models.URLField(null=True, blank=True)
-    turno_almoco = models.BooleanField(default=False)
-    turno_jantar = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    cargos = models.ManyToManyField(Cargos, through='UtilizadoresCargos')
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=30, null=True, blank=True)
-    username = models.CharField(max_length=30, unique=True)
-    telemovel = models.CharField(max_length=50, null=True, blank=True, unique=True)
-    data_nascimento = models.DateField(null=True, blank=True)
-    genero = models.CharField(
-        max_length=10,
-        null=True,
-        blank=True,
-        choices=[
-            ('M', 'Masculino'),
-            ('F', 'Feminino'),
-            ('O', 'Outro')
-        ],
-    )
-    password = models.CharField(max_length=255, null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    last_login = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
 
     class Meta:
         managed = False
         db_table = 'utilizadores_view'
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
+        return self.username
 
 class UtilizadoresCargos(models.Model):
     id_utilizador_cargo = models.AutoField(primary_key=True)

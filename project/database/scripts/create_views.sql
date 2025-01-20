4f57c571f5059fce8b8a2078fe803488e7101671
j-falcao <100717304+j-falcao@users.noreply.github.com>
2025-01-20 18:06:24 +0000

    separatig views

--- project/database/scripts/create_views.sql
+++ project/database/scripts/create_views.sql
@@ -5,9 +5,7 @@ CREATE OR REPLACE VIEW utilizadores_view AS
 SELECT * FROM utilizadores;
 
 CREATE OR REPLACE VIEW cargos_view AS 
-SELECT c.*, CONCAT(u.first_name, ' ', u.last_name) AS nome_utilizador, u.username, u.url_imagem FROM cargos c
-JOIN utilizadorescargos uc ON uc.id_utilizador = c.id_cargo
-JOIN utilizadores u ON u.id = uc.id_utilizador;
+SELECT * FROM cargos;
 
 CREATE OR REPLACE VIEW utilizadorescargos_view AS
 SELECT * FROM utilizadorescargos;
@@ -25,89 +23,61 @@ CREATE OR REPLACE VIEW opcoes_view AS
 SELECT * FROM opcoes;
 
 CREATE OR REPLACE VIEW itens_view AS
-SELECT i.*, p.nome AS nome_produto, p.preco AS preco_produto FROM itens i
-JOIN produtos_view p ON p.id_produto = i.id_item;
+SELECT * FROM itens;
 
 CREATE OR REPLACE VIEW itenscategorias_view AS
-SELECT ic.*, c.designacao AS designacao_categoria, c.designacao AS nome_item FROM itenscategorias ic
-JOIN itens_view i ON i.id_item = ic.id_item
-JOIN categorias_view c ON c.id_categoria = ic.id_categoria;
+SELECT * FROM itenscategorias;
 
 CREATE OR REPLACE VIEW itenstipos_view AS
-SELECT it.*, i.nome_produto AS nome_item, t.designacao AS designacao_tipo FROM itenstipos it
-JOIN itens_view i ON i.id_item = it.id_item
-JOIN tipos_view t ON t.id_tipo = it.id_tipo;
+SELECT * FROM itenstipos;
 
 CREATE OR REPLACE VIEW itensopcoes_view AS
-SELECT io.*, i.nome_produto AS nome_item, o.designacao AS designacao_opcao FROM itensopcoes io
-JOIN itens_view i ON i.id_item = io.id_item
-JOIN opcoes_view o ON o.id_opcao = io.id_opcao;
+SELECT * FROM itensopcoes;
 
 CREATE OR REPLACE VIEW menus_view AS
-SELECT m.*, p.nome AS designacao_menu FROM menus m
-JOIN produtos_view p ON p.id_produto = m.id_menu;
+SELECT * FROM menus;
 
 CREATE OR REPLACE VIEW itensmenus_view AS
-SELECT im.*, i.nome_produto AS nome_item, m.designacao_menu FROM itensmenus im
-JOIN itens_view i ON i.id_item = im.id_item
-JOIN menus_view m ON m.id_menu = im.id_menu;
+SELECT * FROM itensmenus;
 
 CREATE OR REPLACE VIEW fornecedores_view AS
 SELECT * FROM fornecedores;
 
 CREATE OR REPLACE VIEW ingredientes_view AS
-SELECT i.*, f.nome AS nome_fornecedor FROM ingredientes i
-JOIN fornecedores_view f ON f.id_fornecedor = i.id_fornecedor;
+SELECT * FROM ingredientes; 
 
 CREATE OR REPLACE VIEW utensilios_view AS
-SELECT u.*, f.nome AS nome_fornecedor FROM utensilios u
-JOIN fornecedores_view f ON f.id_fornecedor = u.id_fornecedor;
+SELECT * FROM utensilios;
 
 CREATE OR REPLACE VIEW utensilioscarrinhos_view AS
-SELECT uc.*, u.nome AS designacao_utensilio FROM utensilioscarrinhos uc
-JOIN utensilios_view u ON u.id_utensilio = uc.id_utensilio;
+SELECT * FROM utensilioscarrinhos;
 
 CREATE OR REPLACE VIEW ingredientescarrinhos_view AS
-SELECT ic.*, i.nome AS nome_ingrediente FROM ingredientescarrinhos ic
-JOIN ingredientes_view i ON i.id_ingrediente = ic.id_ingrediente;
+SELECT * FROM ingredientescarrinhos;
 
 CREATE OR REPLACE VIEW carrinhos_view AS
-SELECT c.*, i.nome AS nome_ingrediente, u.nome AS nome_utensilio FROM carrinhos c
-JOIN ingredientescarrinhos_view ic ON ic.id_carrinho = c.id_carrinho
-JOIN ingredientes_view i ON i.id_ingrediente = ic.id_ingrediente
-JOIN utensilioscarrinhos_view uc ON uc.id_carrinho = c.id_carrinho
-JOIN utensilios_view u ON u.id_utensilio = uc.id_utensilio;
+SELECT * FROM carrinhos;
 
 CREATE OR REPLACE VIEW mesas_view AS 
-SELECT m.*, em.designacao AS designacao_estado FROM mesas m
-JOIN estadosmesas_view em ON em.id_estado_mesa = m.id_estado_mesa;
+SELECT * FROM mesas;
 
 CREATE OR REPLACE VIEW reservas_view AS 
-SELECT r.*, m.numero FROM reservas r
-JOIN mesas_view m ON m.id_mesa = r.id_mesa;
+SELECT * FROM reservas;
 
 CREATE OR REPLACE VIEW servicos_view AS 
-SELECT s.*, m.numero FROM servicos s
-JOIN mesas_view m ON m.id_mesa = s.id_mesa;
+SELECT * FROM servicos;
 
 CREATE OR REPLACE VIEW pedidos_view AS 
-SELECT p.* FROM pedidos p
-JOIN servicos_view s ON s.id_servico = p.id_servico;
+SELECT * FROM pedidos;
 
 CREATE OR REPLACE VIEW pedidosprodutos_view AS 
-SELECT pp.*, p.id_servico FROM pedidosprodutos pp
-JOIN pedidos_view p ON p.id_pedido = pp.id_pedido;
+SELECT * FROM pedidosprodutos;
 
 CREATE OR REPLACE VIEW pedidosprodutositensopcoes_view AS 
-SELECT ppio.*, pp.id_produto FROM pedidosprodutositensopcoes ppio
-JOIN pedidosprodutos_view pp ON pp.id_pedido_produto = ppio.id_pedido_produto;
+SELECT * FROM pedidosprodutositensopcoes;
 
 CREATE OR REPLACE VIEW instrucoes_view AS
-SELECT i.*, i.nome AS nome_ingrediente, u.nome AS nome_utensilio FROM instrucoes ins
-JOIN instrucoesingredientes ii ON ii.id_instrucao = ins.id_instrucao
-JOIN ingredientes i ON i.id_ingrediente = ii.id_ingrediente
-JOIN instrucoesutensilios iu ON iu.id_instrucao = ins.id_instrucao
-JOIN utensilios u ON u.id_utensilio = iu.id_utensilio;
+SELECT * FROM instrucoes;
 
 CREATE OR REPLACE VIEW instrucoesingredientes_view AS
 SELECT * FROM instrucoesingredientes;
@@ -122,9 +92,4 @@ CREATE OR REPLACE VIEW ingredientesreceitas_view AS
 SELECT * FROM ingredientesreceitas;
 
 CREATE OR REPLACE VIEW receitas_view AS
-SELECT r.*, i.nome AS designacao_ingrediente, u.nome AS designacao_utensilio FROM receitas r
-JOIN utensiliosreceitas ur ON ur.id_receita = r.id_receita
-JOIN utensilios u ON u.id_utensilio = ur.id_utensilio
-JOIN ingredientesreceitas ir ON ir.id_receita = r.id_receita
-JOIN ingredientes i ON i.id_ingrediente = ir.id_ingrediente
-JOIN instrucoes ins ON ins.id_receita = r.id_receita;
+SELECT * FROM receitas;
---
+++ project/database/scripts/create_views_advanced.sql
@@ -0,0 +1,130 @@
+CREATE OR REPLACE VIEW estadosmesas_view AS 
+SELECT * FROM estadosmesas;
+
+CREATE OR REPLACE VIEW utilizadores_view AS 
+SELECT * FROM utilizadores;
+
+CREATE OR REPLACE VIEW cargos_view AS 
+SELECT c.*, CONCAT(u.first_name, ' ', u.last_name) AS nome_utilizador, u.username, u.url_imagem FROM cargos c
+JOIN utilizadorescargos uc ON uc.id_utilizador = c.id_cargo
+JOIN utilizadores u ON u.id = uc.id_utilizador;
+
+CREATE OR REPLACE VIEW utilizadorescargos_view AS
+SELECT * FROM utilizadorescargos;
+
+CREATE OR REPLACE VIEW produtos_view AS
+SELECT * FROM produtos;
+
+CREATE OR REPLACE VIEW categorias_view AS
+SELECT * FROM categorias;
+
+CREATE OR REPLACE VIEW tipos_view AS
+SELECT * FROM tipos;
+
+CREATE OR REPLACE VIEW opcoes_view AS
+SELECT * FROM opcoes;
+
+CREATE OR REPLACE VIEW itens_view AS
+SELECT i.*, p.nome AS nome_produto, p.preco AS preco_produto FROM itens i
+JOIN produtos_view p ON p.id_produto = i.id_item;
+
+CREATE OR REPLACE VIEW itenscategorias_view AS
+SELECT ic.*, c.designacao AS designacao_categoria, c.designacao AS nome_item FROM itenscategorias ic
+JOIN itens_view i ON i.id_item = ic.id_item
+JOIN categorias_view c ON c.id_categoria = ic.id_categoria;
+
+CREATE OR REPLACE VIEW itenstipos_view AS
+SELECT it.*, i.nome_produto AS nome_item, t.designacao AS designacao_tipo FROM itenstipos it
+JOIN itens_view i ON i.id_item = it.id_item
+JOIN tipos_view t ON t.id_tipo = it.id_tipo;
+
+CREATE OR REPLACE VIEW itensopcoes_view AS
+SELECT io.*, i.nome_produto AS nome_item, o.designacao AS designacao_opcao FROM itensopcoes io
+JOIN itens_view i ON i.id_item = io.id_item
+JOIN opcoes_view o ON o.id_opcao = io.id_opcao;
+
+CREATE OR REPLACE VIEW menus_view AS
+SELECT m.*, p.nome AS designacao_menu FROM menus m
+JOIN produtos_view p ON p.id_produto = m.id_menu;
+
+CREATE OR REPLACE VIEW itensmenus_view AS
+SELECT im.*, i.nome_produto AS nome_item, m.designacao_menu FROM itensmenus im
+JOIN itens_view i ON i.id_item = im.id_item
+JOIN menus_view m ON m.id_menu = im.id_menu;
+
+CREATE OR REPLACE VIEW fornecedores_view AS
+SELECT * FROM fornecedores;
+
+CREATE OR REPLACE VIEW ingredientes_view AS
+SELECT i.*, f.nome AS nome_fornecedor FROM ingredientes i
+JOIN fornecedores_view f ON f.id_fornecedor = i.id_fornecedor;
+
+CREATE OR REPLACE VIEW utensilios_view AS
+SELECT u.*, f.nome AS nome_fornecedor FROM utensilios u
+JOIN fornecedores_view f ON f.id_fornecedor = u.id_fornecedor;
+
+CREATE OR REPLACE VIEW utensilioscarrinhos_view AS
+SELECT uc.*, u.nome AS designacao_utensilio FROM utensilioscarrinhos uc
+JOIN utensilios_view u ON u.id_utensilio = uc.id_utensilio;
+
+CREATE OR REPLACE VIEW ingredientescarrinhos_view AS
+SELECT ic.*, i.nome AS nome_ingrediente FROM ingredientescarrinhos ic
+JOIN ingredientes_view i ON i.id_ingrediente = ic.id_ingrediente;
+
+CREATE OR REPLACE VIEW carrinhos_view AS
+SELECT c.*, i.nome AS nome_ingrediente, u.nome AS nome_utensilio FROM carrinhos c
+JOIN ingredientescarrinhos_view ic ON ic.id_carrinho = c.id_carrinho
+JOIN ingredientes_view i ON i.id_ingrediente = ic.id_ingrediente
+JOIN utensilioscarrinhos_view uc ON uc.id_carrinho = c.id_carrinho
+JOIN utensilios_view u ON u.id_utensilio = uc.id_utensilio;
+
+CREATE OR REPLACE VIEW mesas_view AS 
+SELECT m.*, em.designacao AS designacao_estado FROM mesas m
+JOIN estadosmesas_view em ON em.id_estado_mesa = m.id_estado_mesa;
+
+CREATE OR REPLACE VIEW reservas_view AS 
+SELECT r.*, m.numero FROM reservas r
+JOIN mesas_view m ON m.id_mesa = r.id_mesa;
+
+CREATE OR REPLACE VIEW servicos_view AS 
+SELECT s.*, m.numero FROM servicos s
+JOIN mesas_view m ON m.id_mesa = s.id_mesa;
+
+CREATE OR REPLACE VIEW pedidos_view AS 
+SELECT p.* FROM pedidos p
+JOIN servicos_view s ON s.id_servico = p.id_servico;
+
+CREATE OR REPLACE VIEW pedidosprodutos_view AS 
+SELECT pp.*, p.id_servico FROM pedidosprodutos pp
+JOIN pedidos_view p ON p.id_pedido = pp.id_pedido;
+
+CREATE OR REPLACE VIEW pedidosprodutositensopcoes_view AS 
+SELECT ppio.*, pp.id_produto FROM pedidosprodutositensopcoes ppio
+JOIN pedidosprodutos_view pp ON pp.id_pedido_produto = ppio.id_pedido_produto;
+
+CREATE OR REPLACE VIEW instrucoes_view AS
+SELECT i.*, i.nome AS nome_ingrediente, u.nome AS nome_utensilio FROM instrucoes ins
+JOIN instrucoesingredientes ii ON ii.id_instrucao = ins.id_instrucao
+JOIN ingredientes i ON i.id_ingrediente = ii.id_ingrediente
+JOIN instrucoesutensilios iu ON iu.id_instrucao = ins.id_instrucao
+JOIN utensilios u ON u.id_utensilio = iu.id_utensilio;
+
+CREATE OR REPLACE VIEW instrucoesingredientes_view AS
+SELECT * FROM instrucoesingredientes;
+
+CREATE OR REPLACE VIEW instrucoesutensilios_view AS
+SELECT * FROM instrucoesutensilios;
+
+CREATE OR REPLACE VIEW utensiliosreceitas_view AS
+SELECT * FROM utensiliosreceitas;
+
+CREATE OR REPLACE VIEW ingredientesreceitas_view AS
+SELECT * FROM ingredientesreceitas;
+
+CREATE OR REPLACE VIEW receitas_view AS
+SELECT r.*, i.nome AS designacao_ingrediente, u.nome AS designacao_utensilio FROM receitas r
+JOIN utensiliosreceitas ur ON ur.id_receita = r.id_receita
+JOIN utensilios u ON u.id_utensilio = ur.id_utensilio
+JOIN ingredientesreceitas ir ON ir.id_receita = r.id_receita
+JOIN ingredientes i ON i.id_ingrediente = ir.id_ingrediente
+JOIN instrucoes ins ON ins.id_receita = r.id_receita;

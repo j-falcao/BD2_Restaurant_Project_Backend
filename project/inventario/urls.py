7bc55e591c01a6078ff7ced943ab4ded9e336e38
j-falcao <100717304+j-falcao@users.noreply.github.com>
2025-02-01 23:07:18 +0000

    atualizacao de urls

--- project/inventario/urls.py
+++ project/inventario/urls.py
@@ -2,32 +2,32 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ingredientes/', views.get_post_ingredientes, name='ingredientes'),
-    path('ingredientes/<str:id_ingrediente>', views.update_delete_ingredientes, name='ingrediente'),
+    # Ingredientes
+    path('ingredientes/', views.get_post_ingredientes),
+    path('ingredientes/<str:id_ingrediente>', views.update_delete_ingredientes),
 
-    path('utensilios/', views.get_post_utensilios, name='utensilios'),
-    path('utensilios/<str:id_utensilio>/', views.update_delete_utensilios, name='utensilio'),
+    # Utensilios
+    path('utensilios/', views.get_post_utensilios),
+    path('utensilios/<str:id_utensilio>/', views.update_delete_utensilios),
 
-    path('fornecedores/', views.get_post_fornecedores, name='fornecedores'),
-    path('fornecedores/<str:id_fornecedor>/', views.update_delete_fornecedores, name='fornecedor'),
+    # Fornecedores
+    path('fornecedores/', views.get_post_fornecedores),
+    path('fornecedores/<str:id_fornecedor>/', views.update_delete_fornecedores),
 
-    path('carrinhos/', views.get_carrinhos, name='carrinhos'),
+    # Carrinhos
+    path('carrinhos/', views.get_carrinhos),
+    path('carrinhos/<str:id_carrinho>/ingredientes/', views.get_post_ingredientesCarrinhos),
+    path('ingredientescarrinhos/<str:id_ingrediente_carrinho>/', views.update_delete_ingredientesCarrinhos),
+    path('carrinhos/<str:id_carrinho>/utensilios/', views.get_post_utensiliosCarrinhos),
+    path('utensilioscarrinhos/<str:id_utensilio_carrinho>/', views.update_delete_utensiliosCarrinhos),
 
-    path('ingredientescarrinhos/', views.get_post_ingredientesCarrinhos, name='ingredientescarrinhos'),
-    path('ingredientescarrinhos/<str:id_ingredientescarrinho>/', views.update_delete_ingredientesCarrinhos, name='ingredientescarrinho'),
-
-    path('utensilioscarrinhos/', views.get_post_utensiliosCarrinhos, name='utensilioscarrinhos'),
-    path('utensilioscarrinhos/<str:id_utensilioscarrinho>/', views.update_delete_utensiliosCarrinhos, name='utensilioscarrinho'),
-
-    path('receitas/', views.get_post_receitas, name='receitas'),
-    path('receitas/<str:id_receita>/', views.update_delete_receitas, name='receita'),
-
-    path('ingredientesreceitas/', views.get_post_ingredientesReceitas, name='ingredientesreceitas'),
-    path('ingredientesreceitas/<str:id_ingredientesreceita>/', views.update_delete_ingredientesReceitas, name='ingredientesreceita'),
-
-    path('utensiliosreceitas/', views.get_post_utensiliosReceitas, name='utensiliosreceitas'),
-    path('utensiliosreceitas/<str:id_utensiliosreceita>/', views.update_delete_utensiliosReceitas, name='utensiliosreceita'),
-
-    path('instrucoes/', views.get_post_instrucoes, name='instrucoes'),
-    path('instrucoes/<str:id_instrucao>/', views.update_delete_instrucoes, name='instrucao'),
+    # Receitas
+    path('receitas/', views.get_post_receitas),
+    path('receitas/<str:id_receita>/', views.update_delete_receitas),
+    path('receitas/<str:id_receita>/ingredientes/', views.get_post_ingredientesReceitas),
+    path('ingredientesreceitas/<str:id_ingrediente_receita>/', views.update_delete_ingredientesReceitas),
+    path('receitas/<str:id_receita>/utensilios/', views.get_post_utensiliosReceitas),
+    path('utensiliosreceitas/<str:id_utensilio_receita>/', views.update_delete_utensiliosReceitas),
+    path('receitas/<str:id_receita>/instrucoes/', views.get_post_instrucoes),
+    path('instrucoes/<str:id_instrucao>/', views.update_delete_instrucoes),
 ]

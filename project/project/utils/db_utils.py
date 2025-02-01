88006a9ebaae79259c3a07f11c61b4b1ab234b65
j-falcao <100717304+j-falcao@users.noreply.github.com>
2025-02-01 23:10:10 +0000

    funcao que promove dinamismo da chamada das views (atraves das funcoes estaticas definidas nos models)

---
+++ project/project/utils/db_utils.py
@@ -0,0 +1,28 @@
+from django.db import connection
+
+
+def fetch_from_view(view_name, filters=None):
+    try:
+        query = f"SELECT * FROM {view_name}"
+        params = []
+
+        if filters:
+            filter_clauses = []
+            for column, value in filters.items():
+                if value is not None:
+                    filter_clauses.append(f"{column} = %s")
+                    params.append(int(value) if value.isdigit() else value)
+
+            if filter_clauses:
+                query += " WHERE " + " AND ".join(filter_clauses)
+
+        with connection.cursor() as cursor:
+            cursor.execute(query, params)
+            columns = [col[0] for col in cursor.description]
+            results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            return results[0] if len(results) == 1 else results
+
+    except Exception as e:
+        print(
+            f"Ocorreu um erro ao executar fetch_all_from_view({view_name}): {e}")
+        return []

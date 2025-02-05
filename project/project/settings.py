9ebe5f0ff7a03e35aff306e71a5286de881b234b
j-falcao <100717304+j-falcao@users.noreply.github.com>
2025-02-05 18:01:39 +0000

    w

--- project/project/settings.py
+++ project/project/settings.py
@@ -33,7 +33,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    'http://localhost:5174',
 ]
 CORS_ALLOWED_HEADERS = [
     'authorization',

a2eec6fd47f1cb8f598094727d23b6308305229b
j-falcao <100717304+j-falcao@users.noreply.github.com>
2025-02-11 17:11:41 +0000

    fix auth

--- project/autenticacao/permissoes.py
+++ project/autenticacao/permissoes.py
@@ -3,13 +3,12 @@ from rest_framework.permissions import BasePermission
 class BloquearUtilizadoresNaoAutenticados(BasePermission):
 
     rotas_abertas = [
-        '/autenticacao/login/',
-        '/autenticacao/signup/',
+        '/auth/login/',
+        '/auth/signup/',
     ]
 
     def has_permission(self, request, view):
         # Permitir o acesso a rotas abertas
-        print(request.user)
         if request.path in self.rotas_abertas:
             return True
 
